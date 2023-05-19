85a7bbe20b776e1df550134b4a5ed906aeb7b663
Lucas <32420616+QuixoteCode@users.noreply.github.com>
2023-05-19 21:28:08 +0200

    v4.1-Reloj
    Cambios:
    -Se ha incluido un reloj en el extremo superior de la página, debajo del título "<h1>" y encima del menú de navegación, para ello, se han realizado modificaciones a los ficheros de JS, CSS y HTML.
    -El documento CSS ahora se encuentra en su propia carpeta "estilo" y no en el directorio principal junto con index.html

--- Lucas Fernandez LMSGI Dia del Libro/js/javaScript.js
+++ Lucas Fernandez LMSGI Dia del Libro/js/javaScript.js
@@ -1,3 +1,51 @@
+/* Reloj */
+
+function mostrarTiempo(){
+    var fecha = new Date();
+	
+    var horas = fecha.getHours();
+    var minutos = fecha.getMinutes();
+    var segundos = fecha.getSeconds();
+	
+    var mediodia = "DE LA MAÑANA";
+    
+	/* Hará que la medianoche se muestre como 12 */
+    if(horas == 0){
+        horas = 12;
+    }
+    
+	/* Mostrará las horas de la tarde tras el mediodia */
+    if(horas > 12){
+        horas = horas - 12;
+        mediodia = "DE LA TARDE";
+    }
+    
+	/* Esto añadirá un cero delante del numero cuando este sea 9 o inferior */
+    horas = (horas < 10) ? "0" + horas : horas;
+    minutos = (minutos < 10) ? "0" + minutos : minutos;
+    segundos = (segundos < 10) ? "0" + segundos : segundos;
+    
+	/* Finalmente, esto mostrará las horas, minutos, segundos (separados por dos puntos) y si es por la mañana o por la tarde */
+    var relojContenido = horas + ":" + minutos + ":" + segundos + " " + mediodia;
+	
+	
+    document.getElementById("relojJS").innerText = relojContenido;
+    document.getElementById("relojJS").textContent = relojContenido;
+    
+    setTimeout(mostrarTiempo, 1000);
+    
+}
+
+mostrarTiempo();
+
+
+
+
+
+
+/* Carrusel */
+
+
 var carrusel = document.querySelector('#carruselRecs')
 var imagenes = carrusel.querySelectorAll('#img')
 
